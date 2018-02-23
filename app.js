3325096aaaa7cb97b1385ed345d550ee1d3c6cb5
PS <PS@HECTOR>
2018-02-23 21:03:45 +0530

    app

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 /**
  * http://usejsdoc.org/
  */
+console.log("test");
+/**
+ * Simple S3 bucket operations
+ */
+var AWS = require('aws-sdk');
+
+var s3 = new AWS.S3();
+
+// Bucket names must be unique across all S3 users
+
+var myBucket = 'pritishwebsolutions.com';
+
+var params = {
+		  Bucket: myBucket /* required */
+		};
+		s3.getBucketAccelerateConfiguration(params, function(err, data) {
+		  if (err) console.log(err, err.stack); // an error occurred
+		  else    { 
+			  	console.log(data.toString());
+			  	console.log('success');
+			  	params.Key = 'index.html';  	
+			  	getObject();
+		  }           // successful response
+		});
+		
+		function getObject(){
+		 s3.getObject(params, function(err, data) {
+			   if (err) console.log(err); // an error occurred
+			  else     {
+				  var data1 = data;
+				  console.log(data1.Body.toString());
+			  }          // successful response
+			 });		
+		 }
