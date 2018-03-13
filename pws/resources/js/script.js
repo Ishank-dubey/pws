c45238af378014256a7f91a940834797e72aea38
PS <PS@HECTOR>
2018-03-14 02:15:13 +0530

    hp

--- pws/resources/js/script.js
+++ pws/resources/js/script.js
@@ -1,6 +1,43 @@
 $(document).ready(function() {
-    var heightBody = $('body').outerHeight(),
-	height = heightBody - $('nav').outerHeight();
-	$('.aboutusSection').outerHeight(height);
-	$('.projectroadmapSection').outerHeight(heightBody);
+	
+	
+	changeHeightOfSections();
+	scrollFunction(window.location.hash);
+	var timerId;
+	$(window).resize(function(){
+		if(timerId){
+			clearTimeout(timerId);
+		}
+		timerId = setTimeout(changeHeightOfSections,100)
+	});	
+	function changeHeightOfSections() {
+		var heightBody = $('body').outerHeight(),
+		height = heightBody - $('nav').outerHeight();
+		$('.aboutusSection').outerHeight(height);
+		$('.projectroadmapSection').outerHeight(heightBody);
+		
+		
+			var hlocation = window.location.hash;
+	    	if (hlocation) {
+	    		scrollFunction(hlocation);
+	    	}
+		
+		
+	}
+	
+	function scrollFunction (hash) {
+		if (hash !== "") {
+			$('html, body').animate({
+		        scrollTop: $(hash).offset().top - $('body').offset().top + $('body').scrollTop()
+		      }, 800, function(){
+		    	  window.location.hash = hash;
+		        });   
+		};
+	}
+	$("a").on('click', function (event){
+		event.preventDefault();
+		scrollFunction(this.hash);  
+	      
+	});	      
+	
 });
