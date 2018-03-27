1c0d56a19f53ff6887dcc7c379ff0ad1558f8b36
PS <PS@HECTOR>
2018-03-27 22:54:30 +0530

    hp

--- pws/resources/js/script.js
+++ pws/resources/js/script.js
@@ -1,55 +1,55 @@
-$(document).ready(function() {
-	
-    
-    changeHeightOfSections(true);
-	scrollFunction(window.location.hash);
-	var timerId;
-	$(window).scroll(function() {
-	    checkScroll($(this));
-	    });
-	$(window).resize(function(){
-		if(timerId){
-			clearTimeout(timerId);
+(function ($, document, window) {
+	$(document).ready(function() {
+		changeHeightOfSections(true);
+		scrollFunction(window.location.hash);
+		var timerId;
+		$(window).scroll(function() {
+		    checkScroll($(this));
+		    });
+		$(window).resize(function(){
+			if(timerId){
+				clearTimeout(timerId);
+			}
+			timerId = setTimeout(changeHeightOfSections(false),100);
+		});	
+		function checkScroll ($window) {
+			if ( $window.scrollTop() > $window.height()/2 ){
+				$('#scroll-to-top').css('display','block');
+		      }else {
+		    	  $('#scroll-to-top').css('display','none');  
+		      }
 		}
-		timerId = setTimeout(changeHeightOfSections(false),100);
+		function changeHeightOfSections(scroll) {
+			var heightBody = $(window).height(),
+			  height = heightBody - $('nav').outerHeight();
+			$('.aboutusSection').outerHeight(height);
+			$('.projectroadmapSection').outerHeight(heightBody);
+			$('.contactUsSection').outerHeight(heightBody);
+			
+			if(scroll){
+				var hlocation = window.location.hash;
+		    	if (hlocation) {
+		    		scrollFunction(hlocation);
+		    	}
+			}
+			
+		}
+		
+		function scrollFunction (hash) {
+			if (hash !== "") {
+				$('html, body').animate({
+			        scrollTop: $(hash).offset().top - $('body').offset().top + $('body').scrollTop()
+			      }, 800, function(){
+			    	  window.location.hash = hash;
+			    	  checkScroll($(window)) 
+			        });   
+			};
+		}
+		$("a").on('click', function (event){
+			event.preventDefault();
+			scrollFunction(this.hash);  
+		      
+		});	      
+		
 	});	
-	function checkScroll ($window) {
-		if ( $window.scrollTop() > $window.height()/2 ){
-			$('#scroll-to-top').css('display','block');
-	      }else {
-	    	  $('#scroll-to-top').css('display','none');  
-	      }
-	}
-	function changeHeightOfSections(scroll) {
-		var heightBody = $(window).height(),
-		  height = heightBody - $('nav').outerHeight();
-		$('.aboutusSection').outerHeight(height);
-		$('.projectroadmapSection').outerHeight(heightBody);
-		$('.contactUsSection').outerHeight(heightBody);
-		
-		if(scroll){
-			var hlocation = window.location.hash;
-	    	if (hlocation) {
-	    		scrollFunction(hlocation);
-	    	}
-		}
-		
-	}
-	
-	function scrollFunction (hash) {
-		if (hash !== "") {
-			$('html, body').animate({
-		        scrollTop: $(hash).offset().top - $('body').offset().top + $('body').scrollTop()
-		      }, 800, function(){
-		    	  window.location.hash = hash;
-		    	  checkScroll($(window)) 
-		        });   
-		};
-	}
-	$("a").on('click', function (event){
-		event.preventDefault();
-		scrollFunction(this.hash);  
-	      
-	});	      
-	
-});
+})(jQuery, document, window);
