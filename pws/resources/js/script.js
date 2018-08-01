4e14d67b01213646c2074c740ba11deaeab74371
PS <PS@HECTOR>
2018-08-01 09:11:12 +0530

    fe still

--- pws/resources/js/script.js
+++ pws/resources/js/script.js
@@ -4,28 +4,19 @@
 		changeHeightOfSections(true);
 		scrollFunction(window.location.hash);
 		var timerId;
-		$(window).scroll(function() {
-		    checkScroll($(this));
-		    });
+		
 		$(window).resize(function(){
 			if(timerId){
 				clearTimeout(timerId);
 			}
 			timerId = setTimeout(changeHeightOfSections(false),100);
 		});	
-		function checkScroll ($window) {
-			if ( $window.scrollTop() > $window.height()/2 ){
-				$('#scroll-to-top').css('display','block');
-		      }else {
-		    	  $('#scroll-to-top').css('display','none');  
-		      }
-		}
 		function changeHeightOfSections(scroll) {
 			var heightBody = $(window).height(),
 			  height = heightBody - $('nav').outerHeight();
 			$('.aboutusSection').outerHeight(height);
-			$('.projectroadmapSection').outerHeight(heightBody);
-			$('.contactUsSection').outerHeight(heightBody);
+			/*$('.projectroadmapSection').outerHeight(heightBody);
+			$('.contactUsSection').outerHeight(heightBody);*/
 			
 			if(scroll){
 				var hlocation = window.location.hash;
@@ -42,14 +33,13 @@
 			        scrollTop: $(hash).offset().top - $('body').offset().top + $('body').scrollTop()
 			      }, 800, function(){
 			    	  window.location.hash = hash;
-			    	  checkScroll($(window)) 
 			        });   
 			};
 		}
 		
 		function formsubmission(event){
 			event.preventDefault();
-			console.log(this.contactUsMessage.value);
+			$(event.target).find('input.btn').val('Sent!');
 			$('.postMessegeStatus').text('Thank you! We will be in touch with you shortly');
 		}
 		
